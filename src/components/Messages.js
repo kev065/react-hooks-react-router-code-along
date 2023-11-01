2972a53886d611e3f730fe33492344c71bf6b2c2
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-01 16:26:54 +0300

    completed code along

---
+++ src/components/Messages.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Messages() {
+  return <h1>Whats up</h1>;
+}
+
+export default Messages;
---
+++ src/components/Signup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Signup() {
+  return <h1>Welcome my boy</h1>;
+}
+
+export default Signup;
